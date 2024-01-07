5a09b83130e0f7eb9dc7925e5534c4670dc5467f
Isamu Umetsu <isamu.umetsu@avinton.com>
2024-01-07 20:10:50 +0900

    Next.js チュートリアル Routing - Pages and Layouts まで

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Next.js',
+}
 
 export default function Page() {
-  return <h1>Hello, Next.js</h1>
+  return '...'
 }
 
 // export default function Home() {
